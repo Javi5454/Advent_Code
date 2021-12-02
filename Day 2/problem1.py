2f81690d89b3f925b74ad8ec6a20a3814805d8de
Javi5454 <javier-gomezlopez@outlook.es>
2021-12-02 11:01:19 +0100

    Day 2 finished

---
+++ Day 2/problem1.py
@@ -0,0 +1,21 @@
+from typing import Match
+
+horizontal=0
+depth = 0
+
+
+import math
+
+with open("input.txt","r") as data:
+    for line in data.readlines():
+        instructions = line.split()
+
+        if instructions[0] == "forward":
+            horizontal += int(instructions[1])
+        elif instructions[0] == "down":
+            depth += int(instructions[1])
+        else:
+            depth -= int(instructions[1])
+
+print("Your position (horizontal,depth): (", horizontal, ",", depth, ")", sep="")
+print("Result: ", horizontal*depth)
---
+++ Day 2/problem2.py
@@ -0,0 +1,23 @@
+from typing import Match
+
+horizontal=0
+depth = 0
+aim = 0
+
+
+import math
+
+with open("input.txt","r") as data:
+    for line in data.readlines():
+        instructions = line.split()
+
+        if instructions[0] == "forward":
+            horizontal += int(instructions[1])
+            depth += aim * int(instructions[1])
+        elif instructions[0] == "down":
+            aim += int(instructions[1])
+        else:
+            aim -= int(instructions[1])
+
+print("Your position (horizontal,depth): (", horizontal, ",", depth, ")", sep="")
+print("Result: ", horizontal*depth)
