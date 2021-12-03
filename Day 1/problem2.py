f7196328072def15174989b97bc31a6398648c52
Javi5454 <javier-gomezlopez@outlook.es>
2021-12-03 13:59:24 +0100

    Day 1 optimized

--- Day 1/problem2.py
+++ Day 1/problem2.py
@@ -3,9 +3,6 @@ import math
 group = 4*[math.inf]
 counter = 0
 
-check_sum=0;
-check = False
-
 with open("input.txt","r") as archive:
     for line in archive.readlines():
         group = group[1:] + [int(line)]
